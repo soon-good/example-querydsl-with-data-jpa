eec40d317f4271f2f7b21f07ea279f15dfad3934
sgjung <playmymind@daum.net>
2021-03-23 22:43:20 +0900

    프로젝트 초기구조 커밋

---
+++ src/main/java/io/study/qdsl_and_datajpa/QdslAndDatajpaApplication.java
@@ -0,0 +1,13 @@
+package io.study.qdsl_and_datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QdslAndDatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QdslAndDatajpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/study/qdsl_and_datajpa/QdslAndDatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.qdsl_and_datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QdslAndDatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
